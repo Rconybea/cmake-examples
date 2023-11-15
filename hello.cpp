89586f68836aba4f2f8aac603ec1120488441462
Roland Conybeare <rconybeare@gmail.com>
2023-11-15 12:32:48 -0500

    ex3: + boost::program_options -> hello w/ cmdline args

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,28 @@
 // hello.cpp
 
+#include <boost/program_options.hpp>
 #include <iostream>
 
+namespace po = boost::program_options;
 using namespace std;
 
 int
 main(int argc, char * argv[]) {
-    cout << "Hello, world!\n" << endl;
+    po::options_description po_descr{"Options"};
+    po_descr.add_options()
+        ("help,h",
+         "this help")
+        ("subject,s",
+         po::value<string>()->default_value("world"),
+         "say hello to this subject");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, po_descr), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        cerr << po_descr << endl;
+    } else {
+        cout << "Hello, " << vm["subject"].as<string>() << "!\n" << endl;
+    }
 }
