43feec46710268ed45252ee5a9f5ff39d0d7f29e
Roland Conybeare <rconybeare@gmail.com>
2023-12-13 19:50:15 -0500

    compression: + buffered_inflate_zstream: + owned i/o buffers

---
+++ compression/include/compression/buffered_inflate_zstream.hpp
@@ -0,0 +1,127 @@
+// buffered_inflate_zstream.hpp
+
+#include "inflate_zstream.hpp"
+
+/* Example
+ *
+ *   ifstream zfs("path/to/compressedfile.z", ios::binary);
+ *   buffered_inflate_zstream<char> zs;
+ *   ofstream ucfs("path/to/uncompressedfile");
+ *
+ *   while (!zfs.eof()) {
+ *       span<char> z_span = zs.z_avail();
+ *       if (!zfs.read(z_span.lo(), z_span.size())) {
+ *            error...
+ *       }
+ *       zs.z_produce(z_span.prefix(zfs.gcount()));
+ *
+ *       zs.inflate_chunk();
+ *
+ *       span<char> uc_span = zs.uc_contents();
+ *       ucfs.write(uc_span.lo(), uc_span.size());
+ *
+ *       zs.uc_consume(uc_span);
+ *   }
+ */
+template <typename CharT>
+class buffered_inflate_zstream {
+public:
+    using z_span_type = span<std::uint8_t>;
+    using uc_span_type = span<CharT>;
+    using size_type = std::uint64_t;
+
+public:
+    buffered_inflate_zstream(size_type buf_z = 64UL * 1024UL)
+        : z_in_buf_{buf_z},
+          uc_out_buf_{buf_z}
+        {
+            zs_algo_.provide_output(uc_out_buf_.avail().template cast<std::uint8_t>());
+        }
+
+    std::uint64_t n_in_total() const { return zs_algo_.n_in_total(); }
+    std::uint64_t n_out_total() const { return zs_algo_.n_out_total(); }
+
+    /* space available for more compressed input */
+    z_span_type z_avail() const { return z_in_buf_.avail(); }
+    /* space available for more uncompressed input (output of this object) */
+    uc_span_type uc_avail() const { return uc_out_buf_.avail(); }
+    /* uncompressed content available */
+    uc_span_type uc_contents() const { return uc_out_buf_.contents(); }
+
+    /* after populating some prefix of .z_avail(), make existence of that input known
+     * so that it can be uncompressed
+     */
+    void z_produce(z_span_type const & span) {
+        if (span.size()) {
+            z_in_buf_.produce(span);
+
+            /* note whenever we call .inflate,  we consume from .z_in_buf,
+             * so .z_in_buf and .input_zs are kept synchronized
+             */
+            zs_algo_.provide_input(z_in_buf_.contents());
+        }
+    }
+
+    /* consume some uncompressed input -- allows that buffer space to be reused */
+    void uc_consume(uc_span_type const & span) {
+        if (span.size()) {
+            uc_out_buf_.consume(span);
+        }
+
+        if (uc_out_buf_.empty()) {
+            /* can recycle output */
+            zs_algo_.provide_output(uc_out_buf_.avail().template cast<std::uint8_t>());
+        }
+    }
+
+    void uc_consume_all() { this->uc_consume(this->uc_contents()); }
+
+    size_type inflate_chunk() {
+        if (zs_algo_.have_input()) {
+            //zs_algo_.provide_output(reinterpret_cast<uint8_t *>(uc_out_buf_.buf()) + xxx buf_pos,
+            //                         uc_out_buf_.buf_z() - xxx buf_pos);
+
+            std::pair<z_span_type, z_span_type> x = zs_algo_.inflate_chunk2();
+
+            z_in_buf_.consume(x.first);
+            uc_out_buf_.produce(x.second.template cast<CharT>());
+
+            return x.second.size();
+        } else {
+            return 0;
+        }
+    }
+
+    void swap (buffered_inflate_zstream & x) {
+        std::swap(z_in_buf_, x.z_in_buf_);
+        std::swap(zs_algo_, x.zs_algo_);
+        std::swap(uc_out_buf_, x.uc_out_buf_);
+    }
+
+    buffered_inflate_zstream & operator= (buffered_inflate_zstream && x) {
+        z_in_buf_ = std::move(x.z_in_buf_);
+        zs_algo_ = std::move(x.zs_algo_);
+        uc_out_buf_ = std::move(x.uc_out_buf_);
+
+        return *this;
+    }
+
+private:
+    /* compressed input */
+    buffer<std::uint8_t> z_in_buf_;
+
+    /* inflation-state (holds zlib z_stream) */
+    inflate_zstream zs_algo_;
+
+    /* uncompressed input */
+    buffer<CharT> uc_out_buf_;
+};
+
+namespace std {
+    template <typename CharT>
+    void swap(buffered_inflate_zstream<CharT> & lhs,
+              buffered_inflate_zstream<CharT> & rhs)
+    {
+        lhs.swap(rhs);
+    }
+}
