c062e73cd14a7cd3ba75ab8a70a82a4e54774bf0
Roland Conybeare <rconybeare@gmail.com>
2024-02-12 14:57:17 -0500

    zstream utest: suppress console output

--- zstream/utest/zstream.test.cpp
+++ zstream/utest/zstream.test.cpp
@@ -5,6 +5,9 @@
 using namespace std;
 
 TEST_CASE("zstream", "[zstream]") {
+    /* true to enable some logging,  useful if this unit test should fail */
+    constexpr bool c_debug_flag = false;
+
     /* make some buffer space */
     using zbuf_type = array<char, 64*1024>;
     unique_ptr<zbuf_type> zbuf(new zbuf_type());
@@ -23,22 +26,26 @@ TEST_CASE("zstream", "[zstream]") {
         /* reminder: have to close zstream to get complete compressed output. */
         zs.close();
 
-        cout << "uc out: " << zs.rdbuf()->n_uc_out_total() << endl;
-        cout << "z  out: " << zs.rdbuf()->n_z_out_total() << endl;
+        if (c_debug_flag) {
+            cout << "uc out: " << zs.rdbuf()->n_uc_out_total() << endl;
+            cout << "z  out: " << zs.rdbuf()->n_z_out_total() << endl;
+        }
 
-        size_t i = 0;
         size_t n = zs.rdbuf()->n_z_out_total();
 
-        while (i < n) {
-            /* 64 hex values */
-            do {
-                uint8_t ch = (*zbuf)[i];
+        if (c_debug_flag) {
+            size_t i = 0;
+            while (i < n) {
+                /* 64 hex values */
+                do {
+                    uint8_t ch = (*zbuf)[i];
 
-                cout << " " << ::hex(ch);
-                ++i;
-            } while ((i < n) && (i % 64 != 0));
+                    cout << " " << ::hex(ch);
+                    ++i;
+                } while ((i < n) && (i % 64 != 0));
 
-            cout << endl;
+                cout << endl;
+            }
         }
 
         n_z_out_total = n;
@@ -55,7 +62,6 @@ TEST_CASE("zstream", "[zstream]") {
         unique_ptr<zbuf_type> zbuf2(new zbuf_type());
         std::fill(zbuf2->begin(), zbuf2->end(), '\0');
 
-        cerr << "input" << endl;
         unique_ptr<zbuf_type> ucbuf2(new zbuf_type());
         std::fill(ucbuf2->begin(), ucbuf2->end(), '\0');
 
@@ -64,8 +70,8 @@ TEST_CASE("zstream", "[zstream]") {
 
         CHECK(n_read == static_cast<streamsize>(strlen(Text::s_text) + 1));
 
-        cerr << "uncompressed input:" << endl;
-        cerr << string_view(&((*ucbuf2)[0]), &((*ucbuf2)[n_read])) << endl;
+        INFO("uncompressed input:");
+        INFO(string_view(&((*ucbuf2)[0]), &((*ucbuf2)[n_read])));
 
         for (streamsize i=0; i<n_read-1; ++i) {
             INFO(tostr("i=", i, ", s_text[i]=", Text::s_text[i], ", ucbuf2[i]=", (*ucbuf2)[i]));
